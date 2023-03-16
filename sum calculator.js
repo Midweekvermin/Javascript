cf96821ce74111128741b2200e4e04124bded542
Midweekvermin <113259156+Midweekvermin@users.noreply.github.com>
2023-03-16 06:39:38 -0600

    Add files via upload

---
+++ sum calculator.js
@@ -0,0 +1,5 @@
+alert("Simple sum calculator")
+var first = prompt("Enter first number");
+var second = prompt("Enter second number");
+var sum = Number(first) + Number(second);
+alert("the sum is: " + sum);
